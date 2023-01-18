c29813e5709e15a30d0caaa5bd6797cd9162efe3
Reinaldo Silva <reinaldoblack@hotmail.com>
2023-01-18 10:48:05 -0300

    bibliotecas

---
+++ CreditCardFraud.py
@@ -0,0 +1,10 @@
+# Importar Bibliotecas
+
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score, roc_curve
+%matplotlib inline
